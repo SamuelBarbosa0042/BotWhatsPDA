f91495a8f434bf07f1f4fef9914fd69b111d8e57
Samuel Barbosa <79980951+SamuelBarbosa0042@users.noreply.github.com>
2023-05-04 12:20:23 -0300

    commit1

---
+++ knexfile.js
@@ -0,0 +1,54 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/core/database/dev.sqlite3'
+    },
+    migrations:{
+      directory: "./src/core/database/migrations"
+    },
+    seeds:{
+      directory: "./src/core/database/seeds"
+    },
+    useNullAsDefault: true
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
