debbeda20ae48d492fc7933a8ee6bd2407569c05
Samuel Barbosa <79980951+SamuelBarbosa0042@users.noreply.github.com>
2023-05-23 11:30:57 -0300

    commit de terça

--- src/dialogs/templates.js
+++ src/dialogs/templates.js
@@ -1,9 +1,10 @@
 class template{
     static menu(opcoes){
-        return `Olá, eu sou \`\`\`Manuel\`\`\`, por favor selecione \n\n${opcoes.map(opcao => `*${opcao.Codigo_Menu}* - ${opcao.Menu}\n`)}`
+        console.log(opcoes)
+        return `Olá, eu sou \`\`\`Manuel\`\`\`, por favor selecione \n\n${opcoes.map(opcoes => `*${opcoes.Codigo_Menu}* - ${opcoes.Menu}\n`)}`
     }
     static ponto(){
-        return 'insire no formato, separando por asteriscos *\n\n  comentario*quantidade_de_horas*numero_chamado(card)*data(DD/MM/YYYY)\n\n exemplo : \n\ncomentario*2*3333*0805/2023'
+        return 'insira no formato, separando por asteriscos *\n\n  comentario*quantidade_de_horas*numero_chamado(card)*data(DD/MM/YYYY)\n\n exemplo : \n\ncomentario*2*3333*08/05/2023'
     }
     static excluir(){
         return 'Informe qual hora você deseja excluir'
@@ -12,14 +13,34 @@ class template{
 
     }
     static perfil(perfil){
-        console.log('entra aqui')
         return `${perfil.map(perfil => ` Usuario: *${perfil.usuario}*\n\nNumero *${perfil.numero}*\n\nE-mail : ${perfil.email}`)}`
     }
-    static assunto(assunto){
-        return
+    static assunto(nome,mes){
+        const assunto = `Relatório horas extras ${mes} - ${nome}` // inserir mês
+        return assunto
     }
-    static body(body){
-        return
+    static body(horas,nome){
+        
+        let corpo = `<p>Prezados,</p>\n` +
+                    `<p>Segue anexo as informações a respeito das horas extras no mes em questão</p>\n`+
+                    `<p>funcionário : ${nome}</p>\n\n`+
+                    `<p>Total de horas : ${horas}</p>\n\n\n\n`+
+                    `<p>Atenciosamente,\n</p>`+
+                    `<p>BotPDA</p>`
+                    
+        
+        return corpo
+    }
+    static welcome(){
+        let welcome = `Selecione uma das opções:\n\n`+
+                      `*1* - Cadastrar hora extra.\n`+
+                      `*2* - excluir hora extra.\n`+
+                      `*3* - ver horas extras\n`+
+                      `*4* - enviar horas extras ao HEAD.\n`+
+                      `*5* - enviar horas aprovadas ao financeiro.`+
+                      `*0* - finaliza o atendimento`
+
+        return welcome
     }
 }
 
